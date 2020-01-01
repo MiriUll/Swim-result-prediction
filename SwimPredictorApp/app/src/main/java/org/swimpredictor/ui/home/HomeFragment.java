6ed032a1ce3205b11a5c47f054ced2b485923f19
Miri <miri.ull.73@gmail.com>
2020-01-01 12:07:13 +0100

    update layout of home prediction fragment

--- SwimPredictorApp/app/src/main/java/org/swimpredictor/ui/home/HomeFragment.java
+++ SwimPredictorApp/app/src/main/java/org/swimpredictor/ui/home/HomeFragment.java
@@ -1,17 +1,19 @@
 package org.swimpredictor.ui.home;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
+
 import org.swimpredictor.R;
 import org.tensorflow.lite.Interpreter;
 
@@ -23,8 +25,10 @@ import java.util.Objects;
 public class HomeFragment extends Fragment {
 
     private Interpreter model100m;
+    private Interpreter model200m;
 
     private TextView time100m;
+    private TextView time200m;
     private TextView time_50m;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -32,7 +36,8 @@ public class HomeFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        time100m = root.findViewById(R.id.random_textview);
+        time100m = root.findViewById(R.id.textview_100m);
+        //time200m = root.findViewById(R.id.textview_200m);
         time_50m = root.findViewById(R.id.time_50m);
         // load latest stopwatch result if there is one
         final Context context = getActivity();
@@ -42,7 +47,38 @@ public class HomeFragment extends Fragment {
         if (latestStopwatch > 0)
                 time_50m.setText(toHumanReadbleTime(latestStopwatch));
 
-        loadModelFromAssets();
+        model100m = loadModelFromAssets("model100m.tflite");
+        model200m = loadModelFromAssets("model200m.tflite");
+
+        Button predict = root.findViewById(R.id.predict);
+        predict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float[][] input = new float[1][4];
+                // read the values from input
+                String gender = ((Spinner) getView().findViewById(R.id.gender)).getSelectedItem().toString();
+                input[0][0] = (float) (gender.equals("female") ? 1:0);
+                input[0][1] = Float.parseFloat(((Spinner) getView().findViewById(R.id.age)).getSelectedItem().toString());
+                input[0][2] = Float.parseFloat(((Spinner) getView().findViewById(R.id.training_age)).getSelectedItem().toString());
+                //input[0][3] = (float) 36.80;
+                if(time_50m.getText().toString().equals("")){
+                    new AlertDialog.Builder(getActivity())
+                            .setTitle("Error")
+                            .setMessage("Please enter the 50m time!")
+                            .setIcon(android.R.drawable.ic_dialog_info)
+                            .setPositiveButton(android.R.string.ok,null).show();
+                    return;
+                }
+                input[0][3] = toCommonNumberScheme(time_50m.getText().toString());
+                float[][] prediction = new float[1][1];
+                model100m.run(input, prediction);
+                System.out.println((prediction[0][0]));
+                time100m.setText(String.format("%s", toHumanReadbleTime((long) (prediction[0][0])*1000)));
+                model200m.run(input, prediction);
+                System.out.println((prediction[0][0]));
+                time200m.setText(String.format("%s", toHumanReadbleTime((long) (prediction[0][0]*1000))));
+            }
+        });
 
         return root;
     }
@@ -73,11 +109,11 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    private void loadModelFromAssets(){
-        File f = new File(getActivity().getCacheDir()+"/model100m.tflite");
+    private Interpreter loadModelFromAssets(String model){
+        File f = new File(getActivity().getCacheDir()+"/"+model);
         if (!f.exists()) try {
 
-            InputStream is = getActivity().getAssets().open("model100m.tflite");
+            InputStream is = getActivity().getAssets().open(model);
             int size = is.available();
             byte[] buffer = new byte[size];
             is.read(buffer);
@@ -88,23 +124,6 @@ public class HomeFragment extends Fragment {
             fos.write(buffer);
             fos.close();
         } catch (Exception e) { throw new RuntimeException(e); }
-        model100m = new Interpreter(f);
-    }
-
-    public void predictSomeResults(View view){
-        //double prediction = 0.0;
-        //double[] input =  {0, 9, 2, 36.80};
-        //print(model100m.getInputDetails());
-        float[][] input = new float[1][4];
-        // read the values from input
-        String gender = ((Spinner) getView().findViewById(R.id.gender)).getSelectedItem().toString();
-        input[0][0] = (float) (gender.equals("female") ? 1:0);
-        input[0][1] = Float.parseFloat(((Spinner) getView().findViewById(R.id.age)).getSelectedItem().toString());
-        input[0][2] = Float.parseFloat(((Spinner) getView().findViewById(R.id.training_age)).getSelectedItem().toString());
-        //input[0][3] = (float) 36.80;
-        input[0][3] = toCommonNumberScheme(time_50m.getText().toString());
-        float[][] prediction = new float[1][1];
-        model100m.run(input, prediction);
-        time100m.setText(String.format("%s", prediction[0][0]));
+        return new Interpreter(f);
     }
 }
