5a8046a7baff07314d1c1e8559366f0a85e986c9
Miri <miri.ull.73@gmail.com>
2020-01-01 12:07:53 +0100

    update database scheme (add human readable time format string)

--- SwimPredictorApp/app/src/main/java/org/swimpredictor/database/AppDatabase.java
+++ SwimPredictorApp/app/src/main/java/org/swimpredictor/database/AppDatabase.java
@@ -3,7 +3,7 @@ package org.swimpredictor.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities =  {DataSample.class}, version = 1)
+@Database(entities =  {DataSample.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase{
     public abstract SampleDao sampleDao();
 }
