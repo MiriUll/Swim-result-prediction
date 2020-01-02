212c32642ae8a02beec1cc05d18c54eec191202e
Miri <miri.ull.73@gmail.com>
2020-01-02 15:51:57 +0100

    extend visualisation functionalities: add legend and descriptions of plots and remove train_test split for visualisation

--- machine_learning/visualisation/visualise_scatter.py
+++ machine_learning/visualisation/visualise_scatter.py
@@ -3,26 +3,30 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import numpy as np
 
+# change path to "../data_test.csv" for test data
 data = pd.read_csv("../data.csv", sep=';', header=0, dtype={'Gender': int, 'Age': int, 'Competitionage': int,
-                                                         '50m': np.float64, '100m': np.float64, '200m': np.float64})
+                                                            '50m': np.float64, '100m': np.float64, '200m': np.float64})
 X = data.iloc[:, 0:4]
 y = data.iloc[:, 4:6]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-# Convert pandas dataframes into numpy arrays (it is needed for the fitting)
-X_train = X_train.values
-X_test = X_test.values
-y_train100m = y_train.values[:, 0]
-y_train200m = y_train.values[:, 1]
-y_test100m = y_test.values[:, 0]
-y_test200m = y_test.values[:, 1]
-
+# Convert pandas dataframes into numpy arrays
+X_train = X.values
+genders = X_train[:, 0]
+y_train100m = y.values[:, 0]
+y_train200m = y.values[:, 1]
 X_train_50m = X_train[:, 3]
+print('Pearson correlation for 200m: ', np.corrcoef(X_train_50m, y_train200m))
+print('Pearson correlation for 100m: ', np.corrcoef(X_train_50m, y_train100m))
 
-fig,ax = plt.subplots()
-ax.scatter(X_train_50m, y_train100m)
-#ax.scatter(X_train_50m, y_train200m, color='r')
-# for i, lab in enumerate(X_train[:, 1]):
-    #ax.annotate(X_train_50m[i], (X_train_50m[i], y_train100m[i]))
+fig, ax = plt.subplots()
+plt.xlim((25, 55))
+plt.ylim((50, 250))
+plt.xlabel('50m times [s]')
+plt.ylabel('predicted times [s]')
+ax.scatter(X_train_50m, y_train100m, label='100m times')
+ax.scatter(X_train_50m, y_train200m, color='r', label='200m times')
+ax.legend(loc=2)
+
+plt.savefig('/home/anschutzm/Dokumente/TUM/WS1920/Seminar/anschuetz-miriam/machine_learning/plots/training_data.pdf',
+            format='pdf')
 plt.show()
--- machine_learning/visualisation/visualise_utils.py
+++ machine_learning/visualisation/visualise_utils.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def scatter_predictions(y_test, y_predicted):
     fig, ax = plt.subplots()
     # a = plt.axes(aspect='equal')
     plt.scatter(y_test, y_predicted)
-    plt.xlabel('True Values [MPG]')
-    plt.ylabel('Predictions [MPG]')
+    plt.xlabel('True Values')
+    plt.ylabel('Predictions')
     lims = [
         np.min([ax.get_xlim(), ax.get_ylim()]),  # min of both axes
         np.max([ax.get_xlim(), ax.get_ylim()]),  # max of both axes
